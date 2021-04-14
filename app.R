396085f74e0c5baa61279c5ecde7efdb44c17430
ManasaShankaraMurthy <81968681+ManasaShankaraMurthy@users.noreply.github.com>
2021-04-14 17:35:15 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
-library(shiny)
-library(ggplot2)
-library(dplyr)
-library(devtools)
+require(shiny)
+require(ggplot2)
+require(dplyr)
 Loki <- read.csv("https://raw.githubusercontent.com/ManasaShankaraMurthy/ProjectLoki/main/Loki-data.csv")
 
 ui <- fluidPage(
